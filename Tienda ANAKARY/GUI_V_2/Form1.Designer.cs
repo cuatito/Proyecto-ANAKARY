8b2547f5fd99c91063107365395eb8d76dadb83f
cuatito <lachavez201@gmail.com>
2020-10-27 00:30:43 -0600

    Se Hiso cambio en la posicion de inicio del fomrulario

--- Tienda ANAKARY/GUI_V_2/Form1.Designer.cs
+++ Tienda ANAKARY/GUI_V_2/Form1.Designer.cs
@@ -335,6 +335,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MenuVertical.ResumeLayout(false);
